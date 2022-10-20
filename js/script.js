ce50d84d0dd8d366724d6a3e3b24c494c9fa9282
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2022-10-20 18:33:04 +0200

    bonus 3

--- js/script.js
+++ js/script.js
@@ -55,33 +55,70 @@ const ourTeam = [
     }
 
 ]
-const page = document.getElementById('container');
-page.setAttribute('style', `background-color:rgba(57, 145, 7, .4); height:100vh`);
+const page = document.querySelector('body');
+page.setAttribute('style', `background-color:rgba(57, 145, 7, .4)`);
 
 const container = document.getElementById('row');
-for (let teamMate of ourTeam){
-    const cardContainer = document.createElement('div');
-        cardContainer.className = 'col'
-        container.append(cardContainer);
-    const card = document.createElement('div');
-        card.className = 'rounded-2 p-3 d-flex align-items-center justify-content-start';
-        card.setAttribute('style', `background-color:#27BA95`);
-        cardContainer.append(card);
-    const img = document.createElement('img');
-        img.setAttribute('src', `./img/${teamMate.img}`);
-        img.setAttribute('max-width', `100%`);
-        img.setAttribute('width', `30%`);
-        img.className = 'rounded-circle';
-        card.append(img);
-    const cardText = document.createElement('div');
-        cardText.className = 'fw-bold mx-4';
-        card.append(cardText);
-    const name = document.createElement('div');
-        name.innerText = `${teamMate.name}`;
-        name.setAttribute('style', `color:#920025`);
-        cardText.append(name);
-    const role = document.createElement('div');
-        role.innerText = `${teamMate.role}`;
-        role.setAttribute('style', `font-size:.8em; color:#84E3CC`);
-        cardText.append(role);
+const pageCreation = function(){
+    for (let teamMate of ourTeam){
+        const cardContainer = document.createElement('div');
+            cardContainer.className = 'col'
+            container.append(cardContainer);
+        const card = document.createElement('div');
+            card.className = 'rounded-2 p-3 d-flex align-items-center justify-content-start';
+            card.setAttribute('style', `background-color:#27BA95`);
+            cardContainer.append(card);
+        const img = document.createElement('img');
+            img.setAttribute('src', `./img/${teamMate.img}`);
+            img.setAttribute('max-width', `100%`);
+            img.setAttribute('width', `30%`);
+            img.className = 'rounded-circle';
+            card.append(img);
+        const cardText = document.createElement('div');
+            cardText.className = 'fw-bold mx-4';
+            card.append(cardText);
+        const name = document.createElement('div');
+            name.innerText = `${teamMate.name}`;
+            name.setAttribute('style', `color:#920025`);
+            cardText.append(name);
+        const role = document.createElement('div');
+            role.innerText = `${teamMate.role}`;
+            role.setAttribute('style', `font-size:.8em; color:#84E3CC`);
+            cardText.append(role);
+    }
 }
+pageCreation()
+const btns = document.querySelectorAll('button');
+const inputName = document.getElementById('newMateName');
+const inputRole = document.getElementById('newMateRole');
+
+for (let button of btns){
+    button.setAttribute('style', `background-color:#27BA95`);
+}
+
+const showInput = function(){
+    btns[0].classList.add('d-none');
+    btns[1].classList.replace('d-none', 'd-inline-block');
+    inputName.classList.replace('d-none', 'd-inline-block');
+    inputRole.classList.replace('d-none', 'd-inline-block');
+
+    
+}
+const addMember = function(){
+    const member = {
+        name: `${inputName.value}`,
+        role: `${inputRole.value}`,
+        img: 'new-member-avatar.jpg'
+    }
+    ourTeam.push(member)
+    console.log(member,ourTeam)
+    container.innerHTML='';
+    inputName.value='';
+    inputRole.value='';
+    pageCreation()
+}
+
+
+btns[0].addEventListener('click', showInput);
+
+btns[1].addEventListener('click', addMember);
